984e9ed5fa13ff868883b8d05963fa338003bad9
Weslley Rocha <weslley.nx@gmail.com>
2025-12-11 01:46:19 -0300

    Criando estrutura base da api.

---
+++ src/main/java/DeliverIT_ContasAPagar/DeliverItContasAPagarApplication.java
@@ -0,0 +1,13 @@
+package DeliverIT_ContasAPagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverItContasAPagarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverItContasAPagarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DeliverIT_ContasAPagar/DeliverItContasAPagarApplicationTests.java
@@ -0,0 +1,13 @@
+package DeliverIT_ContasAPagar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverItContasAPagarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
