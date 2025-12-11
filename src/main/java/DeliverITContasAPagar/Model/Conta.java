d2f4d02908197f3a81e88fd50b42caa3128ec057
Weslley Rocha <weslley.nx@gmail.com>
2025-12-11 04:11:05 -0300

    Criando Model/Conta.java

---
+++ src/main/java/DeliverITContasAPagar/Model/Conta.java
@@ -0,0 +1,110 @@
+package DeliverITContasAPagar.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "conta")
+public class Conta implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_conta")
+    private Long idConta;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "valor_original",  nullable = false)
+    private BigDecimal valorOriginal;
+
+    @Column(name = "data_vencimento",  nullable = false)
+    private LocalDate dataVencimento;
+
+    @Column(name = "data_pagamento", nullable = false)
+    private LocalDate dataPagamento;
+
+    @Column(name = "valor_corrigido")
+    private BigDecimal valorCorrigido;
+
+    @Column(name = "dias_atraso")
+    private Integer diasAtraso;
+
+    @Column(name = "regra_calculo")
+    private String regraCalculo;
+    
+
+    public Long getIdConta() {
+        return idConta;
+    }
+
+    public void setIdConta(Long idConta) {
+        this.idConta = idConta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getValorOriginal() {
+        return valorOriginal;
+    }
+
+    public void setValorOriginal(BigDecimal valorOriginal) {
+        this.valorOriginal = valorOriginal;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public LocalDate getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(LocalDate dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public BigDecimal getValorCorrigido() {
+        return valorCorrigido;
+    }
+
+    public void setValorCorrigido(BigDecimal valorCorrigido) {
+        this.valorCorrigido = valorCorrigido;
+    }
+
+    public Integer getDiasAtraso() {
+        return diasAtraso;
+    }
+
+    public void setDiasAtraso(Integer diasAtraso) {
+        this.diasAtraso = diasAtraso;
+    }
+
+    public String getRegraCalculo() {
+        return regraCalculo;
+    }
+
+    public void setRegraCalculo(String regraCalculo) {
+        this.regraCalculo = regraCalculo;
+    }
+}
