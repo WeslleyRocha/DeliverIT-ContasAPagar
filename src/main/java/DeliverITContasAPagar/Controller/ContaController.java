f1c73b4c5020d0ba7642411d41c13195e46e5793
Weslley Rocha <weslley.nx@gmail.com>
2025-12-11 17:38:14 -0300

    Criando ContaController, GET / POST.

---
+++ src/main/java/DeliverITContasAPagar/Controller/ContaController.java
@@ -0,0 +1,46 @@
+package DeliverITContasAPagar.Controller;
+
+import DeliverITContasAPagar.Model.Conta;
+import DeliverITContasAPagar.Service.ContaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contas")
+public class ContaController {
+
+    private final ContaService contaService;
+
+    public ContaController(ContaService contaService) {
+        this.contaService = contaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Conta>> listarTodas() {
+
+        List<Conta> contas = contaService.listarTodas();
+
+        return new ResponseEntity<>(contas, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Conta> salvarConta(@RequestBody Conta conta){
+
+        try {
+            Conta contaSalva = contaService.salvar(conta);
+
+            return new ResponseEntity<>(contaSalva, HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
