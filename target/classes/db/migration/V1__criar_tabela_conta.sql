f027fc383a2a28f6de625e5896a213a4962c3518
Weslley Rocha <weslley.nx@gmail.com>
2025-12-12 12:41:34 -0300

    Fix: Docker Compose

---
+++ target/classes/db/migration/V1__criar_tabela_conta.sql
@@ -0,0 +1,11 @@
+CREATE TABLE conta (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+
+    nome VARCHAR(200) NOT NULL,
+    valor_original DECIMAL(19, 2) NOT NULL,
+    data_vencimento DATE NOT NULL,
+    data_pagamento DATE,
+    valor_corrigido DECIMAL(19, 2),
+    dias_atraso INTEGER,
+    regra_calculo VARCHAR(255)
+);
